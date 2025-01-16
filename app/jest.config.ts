c3773121d09602757fc81027417e5319e02553a0
Arvin <Arvin@apples-MacBook-Air.local>
2025-01-16 16:03:19 +0800

    Setup jest environment

--- app/jest.config.ts
+++ app/jest.config.ts
@@ -1,6 +1,23 @@
 import type { Config } from 'jest';
 
-export const config: Config = {
+const config: Config = {
+  rootDir: './',
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.scss$': 'jest-transform-stub',
+  },
+  moduleNameMapper: {
+    '^components/(.*)$': '<rootDir>/components/$1',
+    '^features/(.*)$': '<rootDir>/features/$1',
+    '^pages/(.*)$': '<rootDir>/pages/$1',
+    '^graphql/(.*)$': '<rootDir>/graphql/$1',
+    '^styles/(.*)$': '<rootDir>/styles/$1',
+    '^themes/(.*)$': '<rootDir>/themes/$1',
+    '^utils/(.*)$': '<rootDir>/utils/$1',
+    '^types/(.*)$': '<rootDir>/types/$1',
+    '^enums/(.*)$': '<rootDir>/enums/$1',
+  },
 };
+
+export default config;
