38d72a4450802424d2fddf73c1de05eb72b48101
Arvin <Arvin@apples-MacBook-Air.local>
2025-01-16 14:42:12 +0800

    Create button test id

--- app/components/button/index.tsx
+++ app/components/button/index.tsx
@@ -3,28 +3,28 @@ import { Prop } from './types';
 import classes from 'styles/components/Button.module.scss';
 
 const {
-	wrapper,
-	content,
+  wrapper,
+  content,
 } = classes;
 
 function Button({
-	appendClass,
-	icon,
-	text
+  appendClass,
+  icon,
+  text
 }: Prop) {
-	const {
-		appendWrapper,
-		appendContent,
-	} = appendClass;
-	const wrapperStyle = `${wrapper} ${appendWrapper}`;
-	const contentStyle = `${content} ${appendContent}`;
+  const {
+    appendWrapper,
+    appendContent,
+  } = appendClass;
+  const wrapperStyle = `${wrapper} ${appendWrapper}`;
+  const contentStyle = `${content} ${appendContent}`;
 
-	return (
-		<div className={wrapperStyle}>
-			{icon}
-			<div className={contentStyle}>{text}</div>
-		</div>
-	);
+  return (
+    <div className={wrapperStyle} data-testid="wrapper">
+      {icon}
+      <div className={contentStyle} data-testid="content">{text}</div>
+    </div>
+  );
 };
 
 export default Button;
