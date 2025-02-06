addd66b7391d7e8dac2daaf32901ccba94e05902
Arvin <Arvin@apples-MacBook-Air.local>
2025-02-06 15:02:49 +0800

    Setup frontend i18n setting

---
+++ app/contexts/LocaleContext/index.tsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useEffect, useState, ReactNode } from 'react';
+import { useRouter } from 'next/router';
+import { useCookies } from 'react-cookie';
+
+interface LocaleContextType {
+  locale: string;
+  changeLocale: (newLocale: string) => void;
+}
+
+const LocaleContext = createContext<LocaleContextType | null>(null);
+
+export const LocaleProvider = ({ children }: {
+  children: ReactNode;
+}) => {
+  const router = useRouter();
+  const [cookies, setCookie] = useCookies<"NEXT_LOCALE", {
+    NEXT_LOCALE?: string;
+  }>(['NEXT_LOCALE']);
+  const selectedLocale = cookies.NEXT_LOCALE || 'zh-TW';
+  const [locale, setLocale] = useState(selectedLocale);
+
+  useEffect(() => {
+    setLocale(selectedLocale);
+  }, [selectedLocale]);
+
+  const changeLocale = (newLocale: string) => {
+    setCookie('NEXT_LOCALE', newLocale, { path: '/', maxAge: 365 * 24 * 60 * 60 });
+    setLocale(newLocale);
+    router.push(router.pathname, router.asPath, { locale: newLocale });
+  };
+
+  return (
+    <LocaleContext.Provider value={{ locale, changeLocale }}>
+      {children}
+    </LocaleContext.Provider>
+  );
+}
+
+export const useLocale = (): LocaleContextType => {
+  const context = useContext(LocaleContext);
+  if (!context) {
+    throw new Error('useLocale must be used within a LocaleProvider');
+  }
+  return context;
+}
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,13 +1,25 @@
 import type { AppProps } from 'next/app';
-import { Layout } from 'features';
+import { IntlProvider } from 'next-intl';
+import { useCookies } from 'react-cookie';
 
 import 'styles/globals.scss';
+import { Layout } from 'features';
+import { LocaleProvider } from 'contexts';
 
 function App({ Component, pageProps }: AppProps) {
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+  const [cookies] = useCookies<"NEXT_LOCALE", {
+    NEXT_LOCALE?: string;
+  }>(['NEXT_LOCALE']);
+  const locale = cookies.NEXT_LOCALE || 'zh-TW';
+
+  return (
+    <LocaleProvider>
+      <IntlProvider locale={locale} messages={pageProps.messages}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </IntlProvider>
+    </LocaleProvider>
 	);
 };
 
