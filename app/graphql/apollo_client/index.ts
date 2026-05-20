4890eb9d612b2ba36b3c35396e1d8405f2ddf9c2
Arvin <arvinchen1995@gmail.com>
2026-05-20 09:03:03 +0100

    fix: upgrade node to 22, skip cypress binary and sharp libvips in docker build

--- app/graphql/apollo_client/index.ts
+++ app/graphql/apollo_client/index.ts
@@ -1,11 +1,10 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const SERVER_HOST = process.env.SERVER_HOST;
 const SERVER_PORT = process.env.SERVER_PORT;
 
 export function getApolloClient() {
-	return new ApolloClient({
-		uri: `http://${SERVER_HOST}:${SERVER_PORT}`,
-		cache: new InMemoryCache({}),
-	});
+  return new ApolloClient({
+    uri: `http://server:${SERVER_PORT}`,
+    cache: new InMemoryCache({}),
+  });
 }
