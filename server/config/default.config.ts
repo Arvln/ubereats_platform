a044fea5be69097c0ed0a25ed75987e921df19e9
Arvln <you@example.com>
2026-06-11 09:16:50 -0700

    refector: add port to mysql config

--- server/config/default.config.ts
+++ server/config/default.config.ts
@@ -1,13 +1,14 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 config();
 
 const ENV = process.env;
 
 export const productConfig = {
-	db: {
-		host: ENV.MYSQL_HOST,
-		user: ENV.MYSQL_USER,
-		password: ENV.MYSQL_PASSWORD,
-		database: ENV.DATABASE_NAME
-	}
-}
+  db: {
+    host: ENV.MYSQL_HOST,
+    port: ENV.MYSQL_PORT,
+    user: ENV.MYSQL_USER,
+    password: ENV.MYSQL_PASSWORD,
+    database: ENV.DATABASE_NAME,
+  },
+};
