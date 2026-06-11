bd3721032d19676f16f4570e4c24c16aafa40ebc
Arvln <you@example.com>
2026-06-11 10:33:05 -0700

    merge: merge dev

--- server/config/default.config.ts
+++ server/config/default.config.ts
@@ -6,11 +6,7 @@ const ENV = process.env;
 export const productConfig = {
   db: {
     host: ENV.MYSQL_HOST,
-<<<<<<< HEAD
     port: parseInt(ENV.MYSQL_PORT || "3306", 10),
-=======
-    port: ENV.MYSQL_PORT,
->>>>>>> develop
     user: ENV.MYSQL_USER,
     password: ENV.MYSQL_PASSWORD,
     database: ENV.DATABASE_NAME,
